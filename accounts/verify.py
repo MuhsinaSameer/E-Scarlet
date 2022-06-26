e29fbfa864267f54f568c2c1f5e954d6d3d7ecd0
Muhsina <pvomassery@gmail.com>
2022-06-26 23:34:28 +0530

    updated verify.py

--- accounts/verify.py
+++ accounts/verify.py
@@ -2,8 +2,10 @@ import os
 from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 
-client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-verify = client.verify.services(os.environ['TWILIO_VERIFY_SERVICE_SID'])
+from django.conf import settings
+
+client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+verify = client.verify.services(settings.TWILIO_VERIFY_SERVICE_SID)
 
 
 def send(phone_number):
