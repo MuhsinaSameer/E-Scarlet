cfb9c9513267619862b6abab27a3bcc0f7d150bf
Muhsina <pvomassery@gmail.com>
2022-06-26 21:17:56 +0530

    added allowed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from distutils.command.config import config
 from pathlib import Path
-from decouple import config
+from decouple import config 
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,14 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/asset/")
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
